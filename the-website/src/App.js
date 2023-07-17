9eb823b4f8b6cd4c8c3b009bbfb1918f09611c8d
damg22 <diegoagustin2010@gmail.com>
2023-07-17 16:47:50 -0500

    Hello Sam!!

--- the-website/src/App.js
+++ the-website/src/App.js
@@ -20,7 +20,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Hello Jacob :-{'\)'}
+          Hello Sam :-{'\)'}
         </h1>
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
